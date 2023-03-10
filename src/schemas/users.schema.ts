1c68b6ed06d16b3088fa27777af6d40d29b6b13a
MarkZakelj <markzakelj@gmail.com>
2023-03-10 12:33:44 +0100

    define types and zod schemas for User

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,7 +1,7 @@
-import {z} from "zod";
+import { z } from "zod";
 
 
-export const UserSchema = z.object({
+export const UserBaseSchema = z.object({
     name: z.string({
         required_error: "Name is required and it must be at least 3 characters long"
     }).min(3),
@@ -10,15 +10,20 @@ export const UserSchema = z.object({
     }),
 });
 
-export const UserCreateSchema = UserSchema.merge(z.object({
+export const UserCreateSchema = UserBaseSchema.merge(z.object({
     password: z.string({
         required_error: "Password is requried"
     }).min(8)
 }));
 
-export const UserWithIdSchema = UserSchema.merge(z.object({
+export const UserWithIdSchema = UserBaseSchema.merge(z.object({
     id: z.number()
 }));
+
+//when exporting types, just omit the "Schema" part of the name
+export type UserBase = z.infer<typeof UserBaseSchema>;
+export type UserWithId = z.infer<typeof UserWithIdSchema>;
+export type UserCreate = z.infer<typeof UserCreateSchema>;
 
 
 
