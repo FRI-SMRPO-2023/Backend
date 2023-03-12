db23b7e6cc9f08651180256bb652d3f0879216f1
MarkZakelj <markzakelj@gmail.com>
2023-03-12 13:35:45 +0100

    openapi definitions

--- src/schemas/usersOnProjects.schema.ts
+++ src/schemas/usersOnProjects.schema.ts
@@ -3,6 +3,18 @@ import { RoleInProject } from "@prisma/client"
 import { convertEnum } from '../utils/enum_conversion';
 
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *      RoleInProject:
+ *        type: string
+ *        enum:
+ *          - Developer
+ *          - ProjectManager
+ *          - ProductOwner
+ *          - ScrumMaster
+ */
 
 // for body validation in post request
 export const UsersOnProjectsSchema = z.object({
