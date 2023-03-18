3ebdeb71a8b1afdb9422929949317d05d30a0119
MarkZakelj <markzakelj@gmail.com>
2023-03-18 14:43:28 +0100

    sprint docs

--- src/schemas/sprint.schema.ts
+++ src/schemas/sprint.schema.ts
@@ -1,5 +1,49 @@
 import {z} from "zod";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *      SprintCreate:
+ *        type: object
+ *        properties:
+ *          startDate:
+ *            type: string
+ *          endDate:
+ *            type: string
+ *          speed:
+ *            type: number
+ *        example:
+ *          startDate: "2023-03-01"
+ *          endDate: "2023-04-01"
+ *          speed: 20
+ *        required:
+ *          - startDate
+ *          - endDate
+ *          - speed
+ *      TaskReturn:
+ *        type: object
+ *        properties:
+ *          id:
+ *            type: number
+ *          startDate:
+ *            type: string
+ *          endDate:
+ *            type: string
+ *          speed:
+ *            type: number
+ *        example:
+ *          id: 1
+ *          startDate: "2023-03-01"
+ *          endDate: "2023-04-01"
+ *          speed: 20
+ *        required:
+ *          - id
+ *          - startDate
+ *          - endDate
+ *          - speed
+ */
+
 
 const SprintBaseSchema = z.object({
     startDate: z.date({
