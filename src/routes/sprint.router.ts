1aec1a439a80acfac0eda3486e2a10f5e8e7caa5
MarkZakelj <markzakelj@gmail.com>
2023-04-18 18:45:04 +0200

    docs

--- src/routes/sprint.router.ts
+++ src/routes/sprint.router.ts
@@ -13,6 +13,44 @@ const sprintRouter = express.Router();
 // sprintRouter.route("/current")
 //     .get(SprintController.getCurrent)
 //
+
+
+/**
+ * @openapi
+ * /api/sprint/{id}:
+ *  patch:
+ *      summary: Update any (or multiple) fields in the sprint
+ *      tags: [Sprint]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *      responses:
+ *          200:
+ *              description: Update successful
+ *              content:
+ *                application/json:
+ *                  schema:
+ *                    $ref: '#/components/schemas/SprintReturn'
+ *          409:
+ *              description: Sprint with specified id does not exist error message
+ * 
+ *  delete:
+ *      summary: Delete sprint by id
+ *      tags: [Sprint]
+ *      parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *      responses:
+ *          204:
+ *              description: Delete successful
+ */
+
 sprintRouter.route("/:id")
       .patch(validateSprintUpdate, SprintController.updateSprint)
       .delete(SprintController.deleteSprint)
